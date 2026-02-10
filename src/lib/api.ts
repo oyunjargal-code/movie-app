fe2744b452f26ec8d04754e2c05d27bf2e4b23d3
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-02-10 13:28:42 +0800

    02.10 angid ok push

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,9 +5,9 @@ const baseUrl =
 
 const popularUrl = "/movie/popular?language=en-US&page=1";
 const upcomingUrl = "/movie/upcoming?language=en-US&page=1";
+const topRatedUrl = "/movie/top_rated?language=en-US&page=1";
 
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTU1MDFiMDkwYzg4ZWUxZTk3MTMzNzQyNjczOGJjNyIsIm5iZiI6MTc3MDYwODQ2NS4zMzMsInN1YiI6IjY5ODk1NzUxMTNhMmRjZDNiNThmZDJkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c04TTyCpa_mJ7K1FYuisT2OF3YfIM65zarF2__SUYlQ";
+const token = "";
 
 const options = {
   method: "GET",
@@ -32,10 +32,7 @@ export const getUpcomingMovies = async (): Promise<Response> => {
 };
 
 export const getTopRatedMovies = async (): Promise<Response> => {
-  const respose = await fetch(
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-    options,
-  );
+  const respose = await fetch(`${baseUrl}${topRatedUrl}`, options);
   const data = await respose.json();
 
   return data;
