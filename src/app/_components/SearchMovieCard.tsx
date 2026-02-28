8ec1c8e483fd0a6bc8e48b3074b7c112486a1542
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-02-28 12:23:12 +0800

    02.28 angid

--- src/app/_components/SearchMovieCard.tsx
+++ src/app/_components/SearchMovieCard.tsx
@@ -37,22 +37,22 @@ export const SearchMovieCard = ({ img, rating, name }: SearchMovieType) => {
       <span className="w-full border border-muted-foreground"></span>
     </div>
   );
-  return (
-    <div>
-      <div className="left-2.5">
-        <h5 className="text-sm p-2 truncate">{name}</h5>
-      </div>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${img}`}
-        alt={name}
-        className="w-25 h-12 md:h-25 rounded-t-lg "
-      />
-      <div>
-        <div className="  flex gap-1 p-2">
-          <Star className="w-3 h-3 text-[#FDE047] fill-[#FDE047]" />
-          <span className="text-sm dark:text-[#71717A)]">{rating}/10</span>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <div className="left-2.5">
+  //       <h5 className="text-sm p-2 truncate">{name}</h5>
+  //     </div>
+  //     <img
+  //       src={`https://image.tmdb.org/t/p/w500${img}`}
+  //       alt={name}
+  //       className="w-25 h-12 md:h-25 rounded-t-lg "
+  //     />
+  //     <div>
+  //       <div className="  flex gap-1 p-2">
+  //         <Star className="w-3 h-3 text-[#FDE047] fill-[#FDE047]" />
+  //         <span className="text-sm dark:text-[#71717A)]">{rating}/10</span>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
